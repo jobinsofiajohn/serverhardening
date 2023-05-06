8b1b7850ccc7487ba9641b98a83c377e142fd8d2
jobinsofiajohn <jobinsofiajohn@gmail.com>
2023-05-06 21:02:57 +0530

    chkrootkit

---
+++ CHKrootkit/chkrootkit/commands.sh
@@ -0,0 +1,18 @@
+wget ftp://ftp.chkrootkit.org/pub/seg/pac/chkrootkit.tar.gz
+
+yum install gcc glibc-static
+
+tar -xzf chkrootkit.tar.gz
+mkdir /usr/local/chkrootkit
+mv chkrootkit-0.57/* /usr/local/chkrootkit
+cd /usr/local/chkrootkit
+make sense
+cd
+vim chkrootkit.sh
+
+#!/bin/bash
+cd /usr/local/chkrootkit
+./chkrootkit -q
+//save and exit
+
+./chkrootkit.sh
