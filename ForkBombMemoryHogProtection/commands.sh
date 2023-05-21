0f58978b3cdde8565d7c79cf01fc83b62433c3b6
jobinsofiajohn <jobinsofiajohn@gmail.com>
2023-05-21 10:43:07 +0530

    new

---
+++ ForkBombMemoryHogProtection/commands.sh
@@ -0,0 +1,16 @@
+vim /etc/security/limits.conf
+* hard core 0
+* hard rss 10000
+* hard nproc 200
+save and exit
+
+vim /etc/security/limits.d/20-nproc.conf
+
+*          soft    nproc     100
+save and exit
+
+vim /etc/pam.d/login
+
+session required pam_limits.so
+
+save and exit
