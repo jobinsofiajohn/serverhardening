c3786f91968644a8cab8b357a1149434dce1dba0
jobinsofiajohn <jobinsofiajohn@gmail.com>
2023-05-21 21:37:39 +0530

    new

--- SSHsecuring/firewall/commands.sh
+++ SSHsecuring/firewall/commands.sh
@@ -1,2 +1,2 @@
-firewall-cmd --permanent --zone=public --remove-port=22/tcp #here we are using firewall to block incoming traffics on default port 22 which allowing access through port 2000
+firewall-cmd --permanent --zone=public --remove-port=22/tcp #here we are using firewall to block incoming traffics on default port 22 to make it allow access through another port 2000
 firewall-cmd --reload 
